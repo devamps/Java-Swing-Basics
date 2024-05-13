234250c5634cbc6d64bb2af5c6f2a3c30dd9dcbf
devamps <mateuszkot50@gmail.com>
2024-05-13 23:02:22 +0200

    initial commit

---
+++ src/fundamentals/JOneFrame.java
@@ -0,0 +1,7 @@
+package fundamentals;
+
+import javax.swing.*;
+
+public class JOneFrame extends JFrame {
+
+}
---
+++ src/fundamentals/Main.java
@@ -0,0 +1,11 @@
+package fundamentals;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> new JOneFrame());
+
+    }
+}
