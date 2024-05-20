89f8103eea677460bf58c317247aa0099deb6374
devamps <mateuszkot50@gmail.com>
2024-05-20 22:11:05 +0200

    complete JOneFrame.java - custom notepad

--- src/fundamentals/JOneFrame.java
+++ src/fundamentals/JOneFrame.java
@@ -1,7 +1,25 @@
 package fundamentals;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class JOneFrame extends JFrame {
+public class JOneFrame extends JFrame { //custom notepad
+
+    public JOneFrame() {
+
+        JTextArea jTextArea = new JTextArea();
+        JScrollPane jScrollPane = new JScrollPane(jTextArea);
+
+        jTextArea.setFont(new Font(Font.SERIF, Font.ITALIC, 25));
+        jTextArea.setBackground(Color.BLACK);
+        jTextArea.setForeground(Color.CYAN);
+
+        add(jScrollPane);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
 
 }
